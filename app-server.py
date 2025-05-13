21753d0147622340ea23a6321164e66338679e3e
KozBi <kozlo7830@gmail.com>
2025-05-13 22:39:27 +0200

    Adding new sumbodules for User menagment and messages

--- app-server.py
+++ app-server.py
@@ -1,7 +1,7 @@
 import socket
 import json
 from datetime import datetime
-from my_classes.User_menage import User_menager
+from my_classes.CommandRouter import CommandRouter
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -10,39 +10,8 @@ CREATION_DATE = datetime.now()
 
 class Server:
     def __init__(self):
-        self.User=User_menager()
-        self.commands ={    }
-    response=None
-
-    
-    help_txt_js={"help": "Available command:\n"
-    "'uptime'-return life time of server\n"
-    "'info'-informs about server version\n'stop'-close the server"
-    "\nlogin --your_login-- login to your account."
-    "\ncreate _login_ _password_ - a new account create with a passowrd"}
-
-    def handle_command(self,cmd):
-        
-        user_respond= self.User.handle_user_command(cmd)
-        if user_respond:
-            return user_respond
-        
-        match str(cmd):
-            case "uptime": 
-                #calcute server date
-                current_time=datetime.now()
-                current_time=current_time-CREATION_DATE
-                current_time= str(current_time).split('.')[0]
-                return f"Server uptime: {current_time}"
-            case "info": 
-                return f"Server version: {VERSION}"
-            case "help": 
-                    return self.help_txt_js
-            case "stop": 
-                pass
-            case _: #deafult case
-                return "Uknown command, try 'help'"
-            
+        self.CommandRouter=CommandRouter(VERSION,CREATION_DATE)
+        self.response=None           
 
     def start_server(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -57,7 +26,7 @@ class Server:
                     if not data:
                         break
                     command = data.decode('utf-8') # receive from client a task
-                    self.response = self.handle_command(command)
+                    self.response = self.CommandRouter.handle_command(command)
                     conn.sendall(json.dumps(self.response).encode('utf-8'))
                     
                     if command == "stop":
